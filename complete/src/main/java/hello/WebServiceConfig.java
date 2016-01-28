2d65d2894604b311025453c02098c5705cb60d67
luismoramedina <luismoramedina@gmail.com>
2016-01-28 19:22:15 +0100

    adds wss interceptors to the ws request handling

--- complete/src/main/java/hello/WebServiceConfig.java
+++ complete/src/main/java/hello/WebServiceConfig.java
@@ -5,12 +5,21 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.security.wss4j.Wss4jSecurityInterceptor;
+import org.springframework.ws.soap.security.wss4j.callback.SimplePasswordValidationCallbackHandler;
+import org.springframework.ws.soap.security.xwss.callback.SpringPlainTextPasswordValidationCallbackHandler;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
+
+import java.util.List;
+import java.util.Properties;
 
 @EnableWs
 @Configuration
@@ -37,4 +46,46 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 	public XsdSchema countriesSchema() {
 		return new SimpleXsdSchema(new ClassPathResource("countries.xsd"));
 	}
+
+	/**
+	 * The wss user name interceptor
+     */
+	@Bean
+	public Wss4jSecurityInterceptor wss4jSecurityInterceptor() {
+		Wss4jSecurityInterceptor interceptor = new Wss4jSecurityInterceptor();
+		interceptor.setValidationActions("UsernameToken");
+		interceptor.setValidationCallbackHandler(simplePasswordValidationCallbackHandler());
+		return interceptor;
+	}
+
+	/**
+	 *  Adds a simple username validator callback
+     */
+	@Bean
+	public SimplePasswordValidationCallbackHandler simplePasswordValidationCallbackHandler() {
+		SimplePasswordValidationCallbackHandler simplePasswordValidationCallbackHandler = new SimplePasswordValidationCallbackHandler();
+		Properties properties = new Properties();
+		properties.setProperty("user", "pass");
+		simplePasswordValidationCallbackHandler.setUsers(properties);
+		return simplePasswordValidationCallbackHandler;
+	}
+
+	/**
+	 * Adds security interceptor who validate headers
+     */
+	@Override
+	public void addInterceptors(List<EndpointInterceptor> interceptors) {
+		interceptors.add(wss4jSecurityInterceptor());
+	}
+
+/*
+(NOT USED) A username pass callback handler that uses a {@link AuthenticationManager}
+	@Bean
+	public SpringPlainTextPasswordValidationCallbackHandler springSecurityPasswordValidationCallbackHandler() {
+		SpringPlainTextPasswordValidationCallbackHandler handler = new SpringPlainTextPasswordValidationCallbackHandler();
+		handler.setAuthenticationManager(authentication -> new UsernamePasswordAuthenticationToken("", ""));
+		return handler;
+	}
+*/
+
 }
